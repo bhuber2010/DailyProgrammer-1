7a593875ae2bc00a308e6500b8d82f9a15cffa96
bhuber2010 <bhuber@alumni.colostate.edu>
2015-12-03 12:05:28 -0700

    made it a funciton

--- 03_palindrome/solutions/bh.js
+++ 03_palindrome/solutions/bh.js
@@ -1,22 +1,26 @@
 var prompt = require('prompt');
 
-prompt.start();
 
-prompt.get("word", function (err, result) {
-  var word = result.word;
-  var word_array = word.split("");
-  var reverse = word_array.slice().reverse();
-  console.log(word_array);
-  console.log(reverse);
-  if (word_array[0] !== reverse[0]) {
-    console.log("negative buddy");
-  } else {
-    for (var i = 0; i < word_array.length; i++) {
-      if (word_array[i] !== reverse[i]) {
-        console.log("not a palindrome");
-      } else {
-        console.log("good so far");
+var palindrome = funciton(){
+  prompt.start();
+  prompt.get("word", function (err, result) {
+    var word = result.word;
+    var word_array = word.split("");
+    var reverse = word_array.slice().reverse();
+    console.log(word_array);
+    console.log(reverse);
+    if (word_array[0] !== reverse[0]) {
+      return "negative buddy";
+    } else {
+      for (var i = 0; i < word_array.length; i++) {
+        if (word_array[i] !== reverse[i]) {
+          return "not a palindrome";
+        } else {
+          return "good so far";
+        }
       }
     }
-  }
-});
+  });
+}
+
+palindrome();
