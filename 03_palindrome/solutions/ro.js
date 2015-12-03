1eacc3f4dcdd2420385a327224aa6d803e108703
bhuber2010 <bhuber@alumni.colostate.edu>
2015-12-02 21:46:53 -0700

    palindrome

--- 03_palindrome/solutions/ro.js
+++ 03_palindrome/solutions/ro.js
@@ -5,14 +5,18 @@ prompt.start();
 prompt.get("word", function (err, result) {
   var word = result.word;
   var word_array = word.split("");
+  var reverse = word_array.slice().reverse();
   console.log(word_array);
-  var reverse = word_array.reverse();
   console.log(reverse);
-  for (var i = 0; i < word_array.length; i++) {
-    if (word_array[i] !== reverse[i]) {
-      console.log("not a palindrome");
-    } else {
-      console.log("good so far");
+  if (word_array[0] !== reverse[0]) {
+    console.log("negative buddy");
+  } else {
+    for (var i = 0; i < word_array.length; i++) {
+      if (word_array[i] !== reverse[i]) {
+        console.log("not a palindrome");
+      } else {
+        console.log("good so far");
+      }
     }
   }
-  });
+});
